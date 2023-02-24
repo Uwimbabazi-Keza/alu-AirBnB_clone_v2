5acfd1d1666695a9c46bea1a389214ef43a40d3c
“Uwimbabazi-Keza” <u.keza@alustudent.com>
2023-02-24 02:11:14 -0800

    fix merge issues

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Bases
+from sqlalchemy.orm import relationship
+
+class Amenity(BaseModel, Base):
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
