bef4432711bac9094ce39ddcaec67bee2756df50
Freedisch <freeproduc@gmail.com>
2023-02-24 11:55:15 +0300

    tesr

--- models/state.py
+++ models/state.py
@@ -8,6 +8,6 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    #name = Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state')
 
