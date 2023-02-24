ab9e9aab1536cfb4fcca697423e9dadf4c78556c
“Uwimbabazi-Keza” <u.keza@alustudent.com>
2023-02-24 12:23:28 -0800

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
+    """Amenity class"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship('Place', secondary='place_amenity')
