546a82ec8c743ccd020903c850eca783e2b38d3b
“Uwimbabazi-Keza” <u.keza@alustudent.com>
2023-03-10 08:27:24 -0800

    prepare web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-service nginx start
+sudo service nginx start
 sudo mkdir -p /data/web_static/releases/test/
 sudo echo "<html>
   <head>
@@ -17,6 +17,6 @@ sudo echo "<html>
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
-service nginx restart
-exit 0
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default'
+sudo service nginx restart
+sudo exit 0
