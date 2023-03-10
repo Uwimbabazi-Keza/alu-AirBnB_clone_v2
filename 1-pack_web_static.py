0584c5f12330831da646eacd75f3d0c48783d1f7
“Uwimbabazi-Keza” <u.keza@alustudent.com>
2023-03-10 08:51:15 -0800

    1-pack_web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,7 +7,7 @@ from datetime import date
 def do_pack():
     """generates archive the contents of web_static folder"""
 
-    file_name=strftime("%Y%m%d%H%M%S")
+    file_name = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static/"
